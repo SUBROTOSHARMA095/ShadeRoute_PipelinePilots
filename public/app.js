fa3e11de686bf8b77999513566cf2b68982f0f17
SUBROTO SHARMA <subrotosharma40@gmail.com>
2026-08-14 02:56:24 +0530

    map polygons add

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://api.maptiler.com/maps/streets/style.json?key=YOUR_MAPTILER_KEY',
-    center: [85.8015, 20.2475],
-    zoom: 15,
+    style: '/style.json',
+    /*center: [85.8015, 20.2475],
+    zoom: 15,*/
 });
 
 map.on('load', () => {
@@ -11,8 +11,39 @@ map.on('load', () => {
         .then(response => response.json())
         .then(data => {
 
-            // We'll add the GeoJSON to the map here
+            map.addSource('campus-data', {
+                type: 'geojson',
+                data: data
+            });
 
+            map.addLayer({
+                id: 'campus-fill',
+                type: 'fill',
+                source: 'campus-data',
+                paint: {
+                    'fill-color': '#2f80ed',
+                    'fill-opacity': 0.2
+                }
+            });
+
+            map.addLayer({
+                id: 'campus-outline',
+                type: 'line',
+                source: 'campus-data',
+                paint: {
+                    'line-color': '#2f80ed',
+                    'line-width': 3
+                }
+            });
+            const bounds = new maplibregl.LngLatBounds();
+            data.features.forEach(feature => {
+
+                feature.geometry.coordinates[0].forEach(coordinate => {
+                    bounds.extend(coordinate);
+                });
+            });
+            map.fitBounds(bounds, {
+                padding: 50
+            });
         });
-
 });
